55ec911825f99ce66623b9cf106399a6986a7ba2
Jakub Jankowski <Fenus021@gmail.com>
2026-01-10 21:24:13 +0100

    v2

---
+++ wyklad6i7/zadanie2/fragment.glsl
@@ -0,0 +1,87 @@
+#version 330 core
+
+out vec4 FragColor;
+
+// Dane z Vertex Shadera
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+// Struktura materiału obiektu
+struct Material {
+    float ambient;
+    float diffuse;
+    float specular;
+    float shininess;
+};
+
+// Uniformy oświetlenia
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 lightColor;  // Kolor światła (zmieniany klawiszami 1-4)
+uniform bool useLighting;
+uniform bool useBlinnPhong;
+
+// Uniformy tekstur i koloru obiektu
+uniform sampler2D uTextureSampler;
+uniform bool bUseTexture;
+uniform vec3 uColor;
+
+uniform Material material;
+
+void main()
+{
+    // 1. USTALENIE BAZOWEGO KOLORU OBIEKTU
+    vec4 baseColor;
+    if(bUseTexture) {
+        baseColor = texture(uTextureSampler, TexCoords);
+        if(baseColor.a < 0.1) discard; 
+    } else {
+        baseColor = vec4(uColor, 1.0);
+    }
+
+    // Jeśli oświetlenie wyłączone (F1), zwróć czysty kolor
+    if(!useLighting) {
+        FragColor = baseColor;
+        return;
+    }
+
+    // --- PARAMETR MOCY ŚWIATŁA ---
+    float lightIntensity = 1.8; // Zwiększone dla lepszego efektu
+    vec3 effectiveLightColor = lightColor * lightIntensity;
+
+    // 2. OBLICZENIA OŚWIETLENIA
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 viewDir = normalize(viewPos - FragPos);
+    
+    // Obliczanie odległości dla tłumienia (Attenuation)
+    float distance = length(lightPos - FragPos);
+    // Wzór na tłumienie (stała, liniowa, kwadratowa)
+    // Dopasowane do promienia ok. 20-30 jednostek
+    float attenuation = 1.0 / (1.0 + 0.07 * distance + 0.017 * (distance * distance));
+
+    // AMBIENT (Światło otoczenia - lekko tłumione, by zachować głębię)
+    vec3 ambient = material.ambient * effectiveLightColor * baseColor.rgb;
+
+    // DIFFUSE (Lambert)
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = (material.diffuse * diff) * effectiveLightColor * baseColor.rgb;
+
+    // SPECULAR (Błysk)
+    float spec = 0.0;
+    if(useBlinnPhong) {
+        vec3 halfwayDir = normalize(lightDir + viewDir);
+        spec = pow(max(dot(norm, halfwayDir), 0.0), material.shininess);
+    } else {
+        vec3 reflectDir = reflect(-lightDir, norm);
+        spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    }
+    vec3 specular = (material.specular * spec) * effectiveLightColor;
+
+    // 3. SKŁADANIE FINALNEGO PIKSELA
+    // Tłumienie nakładamy na Diffuse i Specular (Ambient zazwyczaj jest stały)
+    vec3 result = ambient + (diffuse + specular) * attenuation;
+
+    FragColor = vec4(result, baseColor.a);
+}
